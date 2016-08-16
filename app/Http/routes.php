8400c8867e0715f66209562e92556b87ee5ce381
John Hernandez <jhhernandez2.0@gmail.com>
2016-08-16 12:15:12 -0500

    The beggining of our laravel exercises.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,9 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/sayhello/{name}', function ($name) {
-	return "HEY $name! GO FUCK YOURSELF!";
-});
+// Route::get('/sayhello/{name}', function ($name) {
+// 	return "HEY $name! GO FUCK YOURSELF!";
+// });
 
 Route::get('/uppercase/{word}', function ($word) {
 	return strtoupper("$word");
@@ -29,4 +29,37 @@ Route::get('/increment/{number}', function ($number) {
 
 Route::get('/add/{number1}/{number2}', function ($number1, $number2) {
 	return $number1 + $number2;
+});
+
+Route::get('/sayhello/{firstName}/{lastName}', function($firstName, $lastName)
+{
+    if ($firstName == "Chris") {
+        return Redirect::to('/');
+    }
+
+    $data = [
+
+    	'firstName' => $firstName,
+    	'lastName' => $lastName
+
+    ];
+
+    return view('my-first-view', $data);
+});
+
+Route::get('/rolldice/{guess}', function($guess) {
+
+	// $dice = [1, 2, 3, 4, 5, 6];
+
+	$roll = mt_rand(1, 6);
+
+	$data = compact('roll', 'guess');
+
+	// $data = [
+	// 	'roll' => $roll,
+	// 	'guess' => $guess
+
+	// ];
+
+	return view('roll-dice', $data);
 });
---
+++ resources/views/roll-dice.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Dice Roll</title>
+</head>
+<body>
+	<h1>Guess the Roll!</h1><br>
+     <h2>Guess: <?= $guess; ?></h2><br>
+     <h2>Roll: <?= $roll; ?></h2>
+</body>
+</html>
